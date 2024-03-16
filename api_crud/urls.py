6cae4088203805c96b29235e02300f323adbb8f0
roran williams <126867403+roran-williams@users.noreply.github.com>
2024-03-16 08:17:26 +0000

    can acces media folder in during development

--- api_crud/urls.py
+++ api_crud/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 # urls
 urlpatterns = [
@@ -8,3 +10,7 @@ urlpatterns = [
     path('api/roran-williams/auth/', include('authentication.urls')),
     path('admin/', admin.site.urls),
 ]
+
+# Serve media files in development mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

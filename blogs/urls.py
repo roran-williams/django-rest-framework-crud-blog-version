3f817ab8ae7c14567a0675ed4bdef6af3dbbc7ea
roran williams <126867403+roran-williams@users.noreply.github.com>
2024-03-17 16:05:35 +0000

    start filter by topic

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.ListCreateBlogAPIView.as_view(), name='get_post_blogs'),
     path('<int:pk>/', views.RetrieveUpdateDestroyBlogAPIView.as_view(), name='get_delete_update_blog'),
+    path('<str:topic>/', views.ListCreateBlogAPIView.as_view(), name='topic_blog'),  # Endpoint for retrieving blogs by topic
 ]
